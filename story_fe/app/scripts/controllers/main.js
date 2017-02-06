92be9ef54c8fcaecc8a1ca6b85a34ae073803f93
ilan-senexx <ilan@senexx.com>
2017-02-06 11:08:03 +0200

    FE init

---
+++ story_fe/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name storyFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the storyFeApp
+ */
+angular.module('storyFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
