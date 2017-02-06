e0d358de5cdcdcddd59a9c1a17e423b223b753f3
ilan-senexx <ilan@senexx.com>
2017-02-06 16:15:47 +0200

    add background

--- livingstory/app/assets/javascripts/angular/images_controller.js
+++ livingstory/app/assets/javascripts/angular/images_controller.js
@@ -3,5 +3,11 @@ angular.module('story').controller('ImagesCtrl', function($scope) {
   images = ['http://www.kesha3d.com/Gallery/Painting/boy_and_dragon.jpg','http://www.kesha3d.com/Gallery/Amazonian_woman.jpg','http://www.kesha3d.com/Gallery/Tokime.jpg']  
   $scope.images = images;
 
-  $scope.mainImg = 'http://www.kesha3d.com/Gallery/Amazonian_woman.jpg'
+  $scope.mainImg = ['http://www.kesha3d.com/Gallery/Amazonian_woman.jpg']
+  
+  $scope.addImg = function(){
+    var next = $scope.images[Math.floor((Math.random() * 3))];
+    $scope.images.push($scope.mainImg[0]);
+    $scope.mainImg = [next];
+  } 
 });  
