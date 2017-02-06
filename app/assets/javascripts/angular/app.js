ef381437d7cf5875ca359e4187526c52a5be4743
ilan-senexx <ilan@senexx.com>
2017-02-06 18:54:09 +0200

    imageonload fixes

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -6,19 +6,15 @@ var app = angular.module('story', ["ngRoute", 'monospaced.elastic','ngAnimate'])
 //       templateUrl : "main.html"
 //   })
 // });
-app.directive('imageonload', function() {
+app.directive('imageonload', ['$timeout', function($timeout) {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
         element.bind('load', function() {
             $timeout(
               $(".scroller").scrollTop($(".scroller")[0].scrollHeight)
-              console.log 'here' 
-            , 3000)
-        });
-        element.bind('error', function(){
-             
+              ,3000);
         });
     }
   };
-});
+}]);
