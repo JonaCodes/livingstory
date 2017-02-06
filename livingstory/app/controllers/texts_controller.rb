d6ccdad2b0da108ec4e750805bdae42f50a7b194
sxjoel <joel@senexx.com>
2017-02-06 17:34:50 +0200

    Asd

--- livingstory/app/controllers/texts_controller.rb
+++ livingstory/app/controllers/texts_controller.rb
@@ -4,7 +4,7 @@ class TextsController < ActionController::Base
   def create
     puts params['data']
     res = RestClient.post('https://a95d1ab3.ngrok.io/rawText', params['data'].to_json,  {content_type: :json, accept: :json})
-    render json: res
+    render json: {}
   end
 
 end
