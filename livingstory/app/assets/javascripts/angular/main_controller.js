50ab405740ee241ab7e9494725793bbf52300031
sxjoel <joel@senexx.com>
2017-02-06 13:55:13 +0200

    blah

--- livingstory/app/assets/javascripts/angular/main_controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('story').controller('MainCtrl', function($scope) {
-  var title = "Learn AngularJS";
-
-  $scope.title = title;
-  $scope.getTitle = function() {
-    return title;
-  };
-});  
