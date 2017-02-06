63780631361a9b3b491c26882ab173486aab3245
sxjoel <joel@senexx.com>
2017-02-06 18:15:43 +0200

    Blergg

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,7 +4,9 @@ class TextsController < ActionController::Base
   def create
     puts params['data']
     res = RestClient.post('https://a95d1ab3.ngrok.io/rawText', params['data'].to_json,  {content_type: :json, accept: :json})
-    puts res
+    if !res.starts_with?('http')
+      res = 'http://' + res
+    end
     render text: res
 
   end
