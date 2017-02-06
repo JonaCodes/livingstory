69d0eee076460e11ba7f969a9c0688e012ac1756
ilan-senexx <ilan@senexx.com>
2017-02-06 14:32:36 +0200

    application html

--- livingstory/app/assets/javascripts/angular/images_controller.js
+++ livingstory/app/assets/javascripts/angular/images_controller.js
@@ -1,6 +1,7 @@
 angular.module('story').controller('ImagesCtrl', function($scope) {
     
-  images = ['','','']  
-  $scope.images = ;
+  images = ['http://www.kesha3d.com/Gallery/Painting/boy_and_dragon.jpg','http://www.kesha3d.com/Gallery/Amazonian_woman.jpg','http://www.kesha3d.com/Gallery/Tokime.jpg']  
+  $scope.images = images;
 
+  $scope.mainImg = 'http://www.kesha3d.com/Gallery/Amazonian_woman.jpg'
 });  
