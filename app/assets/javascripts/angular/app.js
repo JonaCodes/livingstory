798ed6be73f532361d816bb3e3cea89b5dfa40cc
ilan-senexx <ilan@senexx.com>
2017-02-06 18:44:35 +0200

    imageonload

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -6,3 +6,19 @@ var app = angular.module('story', ["ngRoute", 'monospaced.elastic','ngAnimate'])
 //       templateUrl : "main.html"
 //   })
 // });
+app.directive('imageonload', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+        element.bind('load', function() {
+            $timeout(
+              $(".scroller").scrollTop($(".scroller")[0].scrollHeight)
+              console.log 'here' 
+            , 3000)
+        });
+        element.bind('error', function(){
+             
+        });
+    }
+  };
+});
